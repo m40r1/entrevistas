8ee089b06512f36bc5d29e101f3c78b7a032b72d
m40r1 <99940252+m40r1@users.noreply.github.com>
2023-01-16 20:09:34 +0000

    Separacao do controller facilita leitura
    e testes,mas ainda estou estudando como faze-los.

--- teste/src/main/java/com/gerencimento/pessoa/Pessoa.java
+++ teste/src/main/java/com/gerencimento/pessoa/Pessoa.java
@@ -3,69 +3,52 @@ package com.gerencimento.pessoa;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+class Pessoa {
 
-@RestController
-public class Pessoa {
     private Pessoa(final com.gerencimento.pessoa.pessoaRepo pessoaRepo) {
         this.pessoaRepo = pessoaRepo;
     }
 
     private final pessoaRepo pessoaRepo;
 
-    @PutMapping("/endereco/{id}")
-    void mudarEndereco(@PathVariable final long id, @RequestBody final Endereco endereco) {
+    void mudarEndereco(final long id, final Endereco endereco) {
         // Pegar endereco que a pessoa quer mudar,colocar o novo endereco passado
         // passar o endereco como principal
         // FIXME checar e mudar caso haja outro principal
         pessoaRepo.findById(id).ifPresent(pessoa -> pessoa.enderecos().add(endereco));
     }
 
-    @GetMapping("/pessoa/{nome}")
-    Optional<pessoaVo> acharNome(@PathVariable final String nome) {
+    Optional<pessoaVo> acharNome(final String nome) {
         return pessoaRepo.findByNome(nome);
     };
 
-    @GetMapping("/pessoa")
     List<pessoaVo> consultarPessoas() {
         return pessoaRepo.findAll().toList();
     };
 
-    @PutMapping("/pessoa/{nome}")
-    pessoaVo mudarNome(@RequestParam(value = "nome") final String nome, @PathVariable final String nomeAntigo) {
+    pessoaVo mudarNome(final String nome, final String nomeAntigo) {
         // REVIEW test if map persists the change to the db
         return pessoaRepo.findByNome(nomeAntigo).map(
                 pessoa -> new pessoaVo(pessoa.id(), nome, pessoa.nascimento(), pessoa.enderecos(), pessoa.cidade()))
                 .get();
     };
 
-    @PostMapping("/cadastro/endereco")
-    List<Endereco> criarEndereco(@RequestBody final Endereco endereco,
-            @RequestParam(value = "nome") final String nome) {
+    List<Endereco> criarEndereco(final Endereco endereco, final String nome) {
         // false by default
         pessoaRepo.findByNome(nome).ifPresent(pes -> pes.enderecos().add(endereco));
         return pessoaRepo.findByNome(nome).get().enderecos();
     };
 
-    @PostMapping("/cadastro/pessoa")
-    pessoaVo criarPessoa(@RequestBody final pessoaVo pessoa) {
+    pessoaVo criarPessoa(final pessoaVo pessoa) {
         // REVIEW deal with constructor right?
         return pessoaRepo.save(pessoa);
     };
 
-    @GetMapping("/endereco")
     List<Endereco> listarEnderecos() {
         return pessoaRepo.findAll().flatMap(pes -> pes.enderecos().parallelStream()).toList();
     };
 
-    @GetMapping("/endereco/{nome}")
-    Optional<Endereco> enderecoPrincipal(@PathVariable final String nome) {
+    Optional<Endereco> enderecoPrincipal(final String nome) {
         // REVIEW testar uma forma mais resiliente
         return pessoaRepo.findByNome(nome)
                 .flatMap(pes -> pes.enderecos().stream().filter(endereco -> endereco.principal()).findFirst());
---
+++ teste/src/main/java/com/gerencimento/pessoa/pessoaController.java
@@ -0,0 +1,69 @@
+package com.gerencimento.pessoa;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+class pessoaController {
+    Pessoa pessoa;
+
+    public pessoaController(final Pessoa pessoa) {
+        this.pessoa = pessoa;
+    }
+
+    @PutMapping("/endereco/{id}")
+    void mudarEndereco(@PathVariable final long id, @RequestBody final Endereco endereco) {
+        // Pegar endereco que a pessoa quer mudar,colocar o novo endereco passado
+        // passar o endereco como principal
+        // FIXME checar e mudar caso haja outro principal
+        pessoa.mudarEndereco(id, endereco);
+    }
+
+    @GetMapping("/pessoa/{nome}")
+    Optional<pessoaVo> acharNome(@PathVariable final String nome) {
+        return pessoa.acharNome(nome);
+    };
+
+    @GetMapping("/pessoa")
+    List<pessoaVo> consultarPessoas() {
+        return pessoa.consultarPessoas();
+    };
+
+    @PutMapping("/pessoa/{nome}")
+    pessoaVo mudarNome(@RequestParam(value = "nome") final String nome, @PathVariable final String nomeAntigo) {
+        // REVIEW test if map persists the change to the db
+        return pessoa.mudarNome(nome, nomeAntigo);
+    };
+
+    @PostMapping("/cadastro/endereco")
+    List<Endereco> criarEndereco(@RequestBody final Endereco endereco,
+            @RequestParam(value = "nome") final String nome) {
+        // false by default
+        return pessoa.criarEndereco(endereco, nome);
+    };
+
+    @PostMapping("/cadastro/pessoa")
+    pessoaVo criarPessoa(@RequestBody final pessoaVo dados) {
+        // REVIEW deal with constructor right?
+        return pessoa.criarPessoa(dados);
+    };
+
+    @GetMapping("/endereco")
+    List<Endereco> listarEnderecos() {
+        return pessoa.listarEnderecos();
+    };
+
+    @GetMapping("/endereco/{nome}")
+    Optional<Endereco> enderecoPrincipal(@PathVariable final String nome) {
+        // REVIEW testar uma forma mais resiliente
+        return pessoa.enderecoPrincipal(nome);
+    };
+}
